80b4422a0cc08ea1df5e2f521e358a0bb502bc93
lawrencehan650 <lawrencehan3@gmail.com>
2021-06-29 21:16:02 -0700

    meet the team page

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,8 @@
+export default [
+  require("/Users/lawrencehan/website/node_modules/remark-admonitions/styles/infima.css"),
+  require("/Users/lawrencehan/website/node_modules/remark-admonitions/styles/infima.css"),
+  require("/Users/lawrencehan/website/node_modules/remark-admonitions/styles/infima.css"),
+  require("/Users/lawrencehan/website/node_modules/infima/dist/css/default/default.css"),
+  require("/Users/lawrencehan/website/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
+  require("/Users/lawrencehan/website/src/css/custom.css"),
+];
